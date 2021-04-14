29492d09f831aba5503182cc4e8a3c4782c953f1
Josh Turner <joshaturner1@googlemail.com>
2021-04-14 11:41:32 +0100

    Added more successful builds

--- my-scripts/host/firecracker-run.sh
+++ my-scripts/host/firecracker-run.sh
@@ -3,6 +3,6 @@
 sudo firectl \
     --kernel kernelbuild/$1/vmlinux \
     --root-drive=rootfsbuild/$1.ext2 \
-    --vmm-log-fifo=firelog --ncpus=1 --memory=8192 -d \
+    --ncpus=1 --memory=8192 -d \
     --tap-device=tap100/AA:FC:00:00:00:12 \
     --kernel-opts="console=ttyS0 panic=1 reboot=k pci=off ipv6.disable=1 init=$2"
