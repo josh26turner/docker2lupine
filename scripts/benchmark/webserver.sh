bc0027c5ca8ec27696f9cf9843bb59130177e97d
Josh Turner <joshaturner1@googlemail.com>
2021-04-29 15:15:52 +0100

    Updated webserver benchmark

--- scripts/benchmark/webserver.sh
+++ scripts/benchmark/webserver.sh
@@ -1,65 +1,21 @@
 #!/bin/bash -e
+itr=1000
 
-SCRIPT_DIR=$(dirname $0)/
-
-benchmark_webserver(){
-    APP_NAME=$1
-    DOCKER_IM=$(sed 's/-/:/g' <<< $APP_NAME)
-    echo $APP_NAME
-
-    LOG_DIR='benchlogs/'
-    mkdir -p $LOG_DIR
-
-    run_bench() {
-        TYPE=$1
-        HOST=$2
-        IP=$3
-
-        if [[ $TYPE == "conn" ]]; then
-            ab -n 100000 $IP/index.html > $LOG_DIR$APP_NAME-$HOST-$TYPE.log 2>&1
-        elif [[ $TYPE == "sess" ]]; then
-            ab -k -n 100000 $IP/index.html > $LOG_DIR$APP_NAME-$HOST-$TYPE.log 2>&1
-        fi
-    }
-
-    run_lupine() {
-        TYPE=$1
-
-        python $SCRIPT_DIR/host/host.py $APP_NAME
-
-        while ! grep -q "APP START" firecrackerout/$APP_NAME.log; do
-            sleep 1
-        done
-
-        sleep 5
-
-        run_bench $TYPE lupine 192.168.100.2
-
-        sudo kill -KILL `pgrep -x firecracker`
-    }
-
-    run_docker() {
-        TYPE=$1
-
-        CONTAINER_ID=$(docker run --rm -d $DOCKER_IM)
-
-        CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
-
-        sleep 5
-
-        run_bench $TYPE docker $CONTAINER_IP
-
-        docker stop $CONTAINER_ID
-    }
-
-    python $SCRIPT_DIR/build/build_image.py manifestout/$APP_NAME.json --filesystem > /dev/null 2>&1
-    run_lupine conn
-    python $SCRIPT_DIR/build/build_image.py manifestout/$APP_NAME.json --filesystem > /dev/null 2>&1
-    run_lupine sess
-
-    run_docker conn
-    run_docker sess
+stat() {
+    awk '{x+=$0;y+=$0^2}END{print x/NR, sqrt(y/NR-(x/NR)^2)}'
 }
 
-benchmark_webserver nginx-alpine
-benchmark_webserver httpd-alpine
+run_bench() {
+    for i in `seq $itr`; do
+        ab -n 100 $1/index.html 2>/dev/null | grep 'Requests per second' | sed 's/  */ /g' | cut -d' ' -f 4
+    done | stat
+}
+
+echo "platform: mean variance"
+echo "measured in requests per second, higher better"
+
+echo -n 'lupine: '
+run_bench 192.168.100.2
+
+echo -n 'docker: '
+run_bench 172.17.0.2
